f47388f7cc5cb7ed1fecfbe958fef7af03b6fb43
nguyenloc2003 <nguyenloc31102003@gmail.com>
2025-03-28 18:13:34 +0700

    initial xcode project creation

---
+++ youtubeprojectTests/youtubeprojectTests.swift
@@ -0,0 +1,17 @@
+//
+//  youtubeprojectTests.swift
+//  youtubeprojectTests
+//
+//  Created by mac os on 28/3/25.
+//
+
+import Testing
+@testable import youtubeproject
+
+struct youtubeprojectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
